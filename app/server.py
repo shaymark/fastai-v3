d1d715c5c7971bca2e140154685ec2d8090b9ddb
shay markovich <shaymark@gmail.com>
2019-08-28 22:47:35 +0300

    customized pages

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://github.com/shaymark/dataML/raw/master/export.pkl'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['happy', 'angry']
 path = Path(__file__).parent
 
 app = Starlette()
